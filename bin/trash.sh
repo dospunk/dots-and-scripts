ecc1b3513600b8051f9a0ae868a56673ccf9cea1
CONNOR DOOLEY <dooley@nike.cs.uga.edu>
2017-08-23 15:13:29 -0400

    First commit

---
+++ bin/trash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$1" = "empty" ] ; then
+    echo -n "Are you sure? this action can't be undone! [y/n]"
+    read yn
+    if [ "$yn" = "y" ] ; then
+	rm ~/.trash/*
+	echo "trash emptied"
+    fi
+elif [ "$1" = "un" ] ; then
+    mv "$HOME/.trash/$2" "$PWD"
+else 
+    mv "$1" ~/.trash
+fi
+
